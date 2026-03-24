2bee5ca1496a1b637f42ac96ac700a830fd975b9
Rahul Yadav <130846443+rahulydv1611@users.noreply.github.com>
2026-03-24 23:44:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+
+squares.forEach((square) => {
+ 
+    squares.forEach((s) => {
+      
+      if (s !== square) {
+        s.style.backgroundColor = '#6F4E37';
+      }
+    });
+  });
+
+  
+  square.addEventListener('mouseleave', () => {
+    squares.forEach((s) => {
+      
+      s.style.backgroundColor = '#E6E6FA';
+    });
+  });
+});
